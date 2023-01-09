a66069b4a8d8d447db5040727deaa6cbd78e6bbb
a22marcorr <a22marcorr@192.168.56.1>
2023-01-09 12:40:28 +0100

    123aaaa

--- src/main/java/com/mycompany/cliente_main/Cliente_Main.java
+++ src/main/java/com/mycompany/cliente_main/Cliente_Main.java
@@ -33,7 +33,7 @@ public class Cliente_Main {
                 nifEliminar = leer2.nextLine();
                 BBDD1.eliminarCliente(nifEliminar);
                 System.out.println("Hola mundo");
-                System.out.println("111merdge2");
+                System.out.println("111measdasrawdasdasdasdge2");
             }
 
         } while (respuesta != 0);
