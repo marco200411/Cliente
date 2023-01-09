6736a587f44b1a4885c46e06e5175741057ffd0c
a22marcorr <a22marcorr@192.168.56.1>
2023-01-09 11:27:24 +0100

    Actualizacion hola mundo

--- src/main/java/com/mycompany/cliente_main/Cliente_Main.java
+++ src/main/java/com/mycompany/cliente_main/Cliente_Main.java
@@ -33,6 +33,7 @@ public class Cliente_Main {
                 nifEliminar = leer2.nextLine();
                 BBDD1.eliminarCliente(nifEliminar);
                 System.out.println("Hola mundo");
+                System.out.println("");
             }
 
         } while (respuesta != 0);
